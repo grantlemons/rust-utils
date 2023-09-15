01816db1dff8da53a76db0573d919f84a0946bf1
Grant Lemons <grant0lemons@gmail.com>
2023-09-15 16:21:24 -0600

    add count_occurrences function

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,10 @@ pub fn frequency<T: Sized + Copy + Hash + Eq>(input: &[T]) -> HashMap<T, u64> {
     map
 }
 
+pub fn count_occurrences<T: Sized + Copy + PartialEq>(array: &[T], value: T) -> usize {
+    array.iter().filter(|v| **v == value).count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,5 +33,16 @@ mod tests {
         assert_eq!(*map.get(&15).unwrap(), 2);
         assert_eq!(*map.get(&312).unwrap(), 3);
         assert_eq!(*map.get(&543).unwrap(), 1);
+    }
+
+    #[test]
+    fn count_occurrences_test() {
+        let arr = [
+            1, 534, 312, 543, 22, 43, 22, 1, 15, 312, 312, 534, 876, 15, 22, 1, 534, 43, 14,
+        ];
+
+        assert_eq!(count_occurrences(&arr, 15), 2);
+        assert_eq!(count_occurrences(&arr, 312), 3);
+        assert_eq!(count_occurrences(&arr, 543), 1);
     }
 }
