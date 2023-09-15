efceaeea0e941c9319088dbe65920d2a307a8ac8
Grant Lemons <grant0lemons@gmail.com>
2023-09-15 16:21:09 -0600

    add frequency util

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+use std::hash::Hash;
+
+pub fn frequency<T: Sized + Copy + Hash + Eq>(input: &[T]) -> HashMap<T, u64> {
+    let mut map = HashMap::new();
+
+    for value in input {
+        if let Some(freq) = map.get_mut(value) {
+            *freq += 1;
+        } else {
+            map.insert(*value, 1);
+        }
+    }
+
+    map
 }
 
 #[cfg(test)]
@@ -7,8 +20,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn frequency_test() {
+        let arr = [
+            1, 534, 312, 543, 22, 43, 22, 1, 15, 312, 312, 534, 876, 15, 22, 1, 534, 43, 14,
+        ];
+        let map = frequency(&arr);
+
+        assert_eq!(*map.get(&15).unwrap(), 2);
+        assert_eq!(*map.get(&312).unwrap(), 3);
+        assert_eq!(*map.get(&543).unwrap(), 1);
     }
 }
