5a65249e18328166abb5c6396b0e163bd28dc051
Grant Lemons <grant0lemons@gmail.com>
2023-09-22 13:21:10 -0600

    create sequential bench

--- benches/frequency_bench.rs
+++ benches/frequency_bench.rs
@@ -15,27 +15,47 @@ fn load_from_file(file_path: &str) -> Vec<u16> {
         .collect();
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let numbers_100: &[u16] = &load_from_file("randoms/100randoms.txt");
-    let numbers_1000: &[u16] = &load_from_file("randoms/1000randoms.txt");
-    let numbers_10000: &[u16] = &load_from_file("randoms/10000randoms.txt");
-    let numbers_100000: &[u16] = &load_from_file("randoms/100000randoms.txt");
-    let numbers_1000000: &[u16] = &load_from_file("randoms/1000000randoms.txt");
+fn rand_freq(c: &mut Criterion) {
+    let rand_numbers_100: &[u16] = &load_from_file("randoms/100randoms.txt");
+    let rand_numbers_1000: &[u16] = &load_from_file("randoms/1000randoms.txt");
+    let rand_numbers_10000: &[u16] = &load_from_file("randoms/10000randoms.txt");
+    let rand_numbers_100000: &[u16] = &load_from_file("randoms/100000randoms.txt");
+    let rand_numbers_1000000: &[u16] = &load_from_file("randoms/1000000randoms.txt");
 
-    c.bench_function("freq 100", |b| b.iter(|| frequency(black_box(numbers_100))));
-    c.bench_function("freq 1000", |b| {
-        b.iter(|| frequency(black_box(numbers_1000)))
+    c.bench_function("freq rand 100", |b| {
+        b.iter(|| frequency(black_box(rand_numbers_100)))
     });
-    c.bench_function("freq 10000", |b| {
-        b.iter(|| frequency(black_box(numbers_10000)))
+    c.bench_function("freq rand 1000", |b| {
+        b.iter(|| frequency(black_box(rand_numbers_1000)))
     });
-    c.bench_function("freq 100000", |b| {
-        b.iter(|| frequency(black_box(numbers_100000)))
+    c.bench_function("freq rand 10000", |b| {
+        b.iter(|| frequency(black_box(rand_numbers_10000)))
     });
-    c.bench_function("freq 1000000", |b| {
-        b.iter(|| frequency(black_box(numbers_1000000)))
+    c.bench_function("freq rand 100000", |b| {
+        b.iter(|| frequency(black_box(rand_numbers_100000)))
+    });
+    c.bench_function("freq rand 1000000", |b| {
+        b.iter(|| frequency(black_box(rand_numbers_1000000)))
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+fn seq_freq(c: &mut Criterion) {
+    c.bench_function("freq seq 100", |b| {
+        b.iter(|| frequency(black_box(&(0..100).collect::<Vec<u32>>())))
+    });
+    c.bench_function("freq seq 1000", |b| {
+        b.iter(|| frequency(black_box(&(0..1000).collect::<Vec<u32>>())))
+    });
+    c.bench_function("freq seq 10000", |b| {
+        b.iter(|| frequency(black_box(&(0..10000).collect::<Vec<u32>>())))
+    });
+    c.bench_function("freq seq 100000", |b| {
+        b.iter(|| frequency(black_box(&(0..100000).collect::<Vec<u32>>())))
+    });
+    c.bench_function("freq seq 1000000", |b| {
+        b.iter(|| frequency(black_box(&(0..1000000).collect::<Vec<u32>>())))
+    });
+}
+
+criterion_group!(freq, rand_freq, seq_freq);
+criterion_main!(freq);
